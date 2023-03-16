8272ea43ac3aa3bb5a28522c58a12b2af7ac0655
‘miss-mofokeng’ <mphokhumomofokeng12@gmail.com>
2023-03-16 07:55:28 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * main - a C program that prints using puts function
-n
- *return: 0
+ * main - a C program that uses puts function
+ *
+ * return: 0
  */
-int main(void) {
-	puts("Programming is like building a multilingual puzzle");
-	return 0;
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
