f614898ff00911fd7e62babdb1cbced4933c16f1
‘miss-mofokeng’ <mphokhumomofokeng12@gmail.com>
2023-03-17 09:13:30 -0700

    alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - a program that prints the alphabet in lowercase
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
