06fa0f5474e7bcc6112af2ca4f548a41527374d0
‘miss-mofokeng’ <mphokhumomofokeng12@gmail.com>
2023-03-21 11:40:55 -0700

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a number
+ *
+ * @n: function that parameter
+ *
+ * Return: 0, 1, -1 (success)
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
+
