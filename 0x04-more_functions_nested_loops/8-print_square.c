39abc1ecc59f4705ca191bfa1681c677432f25a6
‘miss-mofokeng’ <mphokhumomofokeng12@gmail.com>
2023-03-23 15:47:35 -0700

    square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,1 +1,29 @@
 #include "main.h"
+
+/**
+ * print_square - a function that prints a shape
+ * @size: a square
+ *
+ * Return: 0.
+ */
+void print_square(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int s;
+		int p;
+
+		for (s = 0 ; s < size ; s++)
+		{
+			for (p = 0 ; p < size ; p++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
