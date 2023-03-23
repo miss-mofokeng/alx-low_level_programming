c61e5c24ce2c21ad12404b41b163056361e869c0
‘miss-mofokeng’ <mphokhumomofokeng12@gmail.com>
2023-03-23 11:36:56 -0700

    The numbers speak for themselves

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,1 +1,17 @@
 #include "main.h"
+
+/**
+ * print_numbers - a function that prints numbers 0 through 9
+ *
+ * Return: 0 (success)
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = '0' ; i <= '9' ; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
