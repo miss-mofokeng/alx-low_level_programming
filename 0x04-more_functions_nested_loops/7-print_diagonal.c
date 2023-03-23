88d4c0cfccf9d5af1c2cd426671686ab5a7caec0
‘miss-mofokeng’ <mphokhumomofokeng12@gmail.com>
2023-03-23 15:24:56 -0700

    parallel universe

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,1 +1,32 @@
 #include "main.h"
+
+/**
+ * print_diagonal - a function that draws a line
+ * @n: diagonal line
+ *
+ * Return 0.
+ */
+void print_diagonal(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int d;
+		int l;
+
+		for (d = 0 ; d < n ; d++)
+		{
+			for (l = 0 ; l < n ; l++)
+			{
+				if (l == d)
+					_putchar('\\');
+				else if (l < d)
+					_putchar(' ');
+			}
+			_putchar('\n');
+		}
+	}
+}
