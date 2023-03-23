e50eee6f24a1c97be1537b851d9d362fe2f94f91
‘miss-mofokeng’ <mphokhumomofokeng12@gmail.com>
2023-03-23 15:32:30 -0700

    straight line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,1 +1,25 @@
 #include "main.h"
+
+/**
+ * print_line - a function that prints a line
+ * @n: striaght line
+ *
+ * Return: 0.
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int l;
+
+		for (l = 1; l <= n ; l++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
