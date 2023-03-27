3987c292dea0f8f9ec706c5106841629a7e75230
‘miss-mofokeng’ <mphokhumomofokeng12@gmail.com>
2023-03-27 09:58:31 -0700

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - A function that takes a pointer and updates the value
+ * @n: int as a parameter and point value to to 98
+ *
+ * Return: 0.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
