7929f7335cb4e25f1602f95f22b089966d2b2fdd
‘miss-mofokeng’ <mphokhumomofokeng12@gmail.com>
2023-04-12 09:48:16 -0700

    wake up and apologize

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * alloc_grid - a function that returns a pointer
+ * @width: first dimension
+ * @height: second dimension
+ *
+ * Return: weight, height or 0
+ */
+int **alloc_grid(int width, int height)
+{
+	int **dim;
+	int x;
+	int y;
+
+	if (width <= 0)
+		return (NULL);
+	if (height <= 0)
+		return (NULL);
+
+	dim = malloc(sizeof(int *) * height);
+
+	if (dim == NULL)
+		return (NULL);
+	for (x = 0; x < height; x++)
+	{
+		dim[x] = malloc(sizeof(int) * width);
+
+		if (dim[x] == NULL)
+		{
+			for (; x >= 0; x--)
+				free(dim[x]);
+
+			free(dim);
+			return (NULL);
+		}
+	}
+	for (x = 0; x < height; y++)
+	{
+		for (y = 0; y < width; y++)
+			dim[x][y] = 0;
+	}
+	return (dim);
+}
